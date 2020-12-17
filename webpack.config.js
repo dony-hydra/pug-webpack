1f568ad2d3fe702978c4d9906f34cbd2e8e9ee94
trantrungisk5 <trantrungisk5@gmail.com>
2020-12-17 11:10:51 +0700

    init source

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const pug = {
+  test: /\.pug$/,
+  use: ['html-loader', 'pug-html-loader']
+};
+
+const config = {
+  entry: './src/app.js',
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [pug]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.pug',
+    //   inject: false
+    })
+ ]
+};
+
+module.exports = config;
