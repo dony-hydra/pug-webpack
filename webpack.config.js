ed52baa6d9472ffdfb2e5454c42703b250377670
trantrungisk5 <trantrungisk5@gmail.com>
2020-12-18 11:45:02 +0700

    [feature] Generating multiple HTML page

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,30 @@
+/* Webpack.config.js*/
 const path = require('path');
-const webpack = require('webpack');
+// const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const fs = require('fs');
+
 const src = path.join(__dirname, 'src');
+
+function generateHtmlPlugins(templateDir){
+    // Read files in template directory
+    const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+    return templateFiles.map(item => {
+        // Split names and extension
+        const parts = item.split('.');
+        const name = parts[0];
+        const extension = parts[1];
+        // Create new HTMLWebpackPlugin with options
+        return new HtmlWebpackPlugin({
+            filename:`${name}.html`,
+            template: path.resolve(__dirname,`${templateDir}/${name}.${extension}`)
+        })
+    })
+}
+
+// Call our function on our views directory
+const htmlPlugins = generateHtmlPlugins('./src/template/views');
 
 const pug = {
   test: /\.pug$/,
@@ -30,6 +52,7 @@ const config = {
     //   inject: false
     })
  ]
+ .concat(htmlPlugins)
 };
 
 module.exports = config;
